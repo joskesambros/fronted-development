37f30576a502bef4d87b505cda2b943768abe74d
joskesambros <joskesambros@gmail.com>
2018-11-06 21:06:14 +0100

    eindje

--- js/heart.js
+++ js/heart.js
@@ -1,6 +1,17 @@
+var hartje = document.getElementById('hartje');
 
-var hartje = document.getElementById('hartje2');
 
 hartje.onclick = function () {
-    hartje.src = '../img/hartje.gif';
-    };
+    if (hartje.src.includes('/img/hart2.jpg')) {
+        hartje.src = '../img/hartje22.gif';
+    } else
+    if (hartje.src.includes('/img/hartje22.gif')) {
+        hartje.src = '../img/hartje23.gif';
+    } else
+    if (hartje.src.includes('/img/hartje23.gif')) {
+        hartje.src = '../img/hartje22.gif';
+    }
+    if (hartje.src.includes('/img/hartgevuld.jpg')) {
+        hartje.src = '../img/hartje23.gif';
+    }
+};
